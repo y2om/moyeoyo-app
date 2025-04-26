e0d667f36b0207f23f4d6472a0756cd5cd2ec562
y2om <ybhss1418@naver.com>
2025-04-27 02:22:00 +0900

    Fix: firebase storageBucket 수정 및 기타 수정사항 반영

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,11 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
+﻿import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCQk7avboGwp0PuNv7ggvXNb-A9M84xxSI",
     authDomain: "moyeoyo-8e39a.firebaseapp.com",
     projectId: "moyeoyo-8e39a",
-    storageBucket: "moyeoyo-8e39a.firebasestorage.app",
+    storageBucket: "moyeoyo-8e39a.appspot.com", // ✅ 수정 완료
     messagingSenderId: "354214304287",
     appId: "1:354214304287:web:21887f648c50e80926132a",
 };
